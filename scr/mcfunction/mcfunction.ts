b0cebd4c4e4da72bfdaca57cd7844097ba733779
DaanV2 <daanverstraten@hotmail.com>
2020-05-30 23:15:54 +0200

    Updating typescript

--- scr/mcfunction/mcfunction.ts
+++ scr/mcfunction/mcfunction.ts
@@ -1,16 +1,12 @@
 import * as vscode from 'vscode';
+import * as McfunctionCompletionItems from './completion_item';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	const provider = vscode.languages.registerCompletionItemProvider(
 		'bc-minecraft-mcfunction',
-		{
-			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-				
-
-			}
-		},
-		" " // triggered whenever a '.' is being typed
+		new McfunctionCompletionItems.McFunctionCompletion(),
+		" " // triggered whenever a ' ' is being typed
 	);
 
     context.subscriptions.push(provider);
