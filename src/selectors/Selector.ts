c26dcfb450be40f80c10ff10104bd13cae1f2b95
DaanV2 <daanverstraten@hotmail.com>
2020-05-31 16:46:36 +0200

    Added mcfunctions

--- src/selectors/Selector.ts
+++ src/selectors/Selector.ts
@@ -1,20 +1,35 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-export function IsInSelector(document: vscode.TextDocument, position: vscode.Position) : boolean {
-    var Line = document.lineAt(position.line);
-    var Text = Line.text;
+export function IsInSelector(document: vscode.TextDocument, position: vscode.Position): boolean {
+  var Line = document.lineAt(position.line);
+  var Text = Line.text;
 
-    for (let index = position.character; index > 2; index--) {
-        var c = Text.charAt(index);
+  for (let index = position.character; index > 2; index--) {
+    var c = Text.charAt(index);
 
-        switch (c){
-            case "]":
-                return false;
+    switch (c) {
+      case "@":
+        return true;
+      case "]":
+        return false;
 
-            case "[":
-                
+      case "[":
+        if (Text.charAt(index - 2) == "@") {
+          var C = Text.charAt(index - 1);
+
+          switch (C) {
+            case "a":
+            case "s":
+            case "e":
+            case "r":
+            case "p":
+              return true;
+          }
         }
-    }
 
-    return false;
+        return false;
+    }
+  }
+
+  return false;
 }
