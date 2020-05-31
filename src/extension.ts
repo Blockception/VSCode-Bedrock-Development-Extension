831c730b5a37b6b9f128fef2f16a84d293be2b11
DaanV2 <daanverstraten@hotmail.com>
2020-05-31 11:03:33 +0200

    Updated

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import * as mcfunction from './mcfunction/mcfunction';
+import * as mcfunction from './mcfunction/main';
 
 export function activate(context: vscode.ExtensionContext) {
 	mcfunction.activate(context); //Add mcfunction
--- src/mcfunction/commands/Scoreboard.ts
+++ src/mcfunction/commands/Scoreboard.ts
@@ -1,25 +1,6 @@
-import * as vscode from 'vscode';
-import * as CompletionFunction from "../CompletionFunctions"
+import * as vscode from "vscode";
+import * as CompletionFunction from "../functions/CompletionFunctions";
 
-export function CompletionItems(Words : string[], receiver: vscode.CompletionList) : void {
-    //Just typed scoreboard
-    if (Words.length <= 1){
-        receiver.items.push(CompletionFunction.createCompletionItem("objectives", "objectives", "The handling of objectives for minecraft"));
-        receiver.items.push(CompletionFunction.createCompletionItem("players", "players", "The handling of players, and operations for minecraft"));
-        return;
-    }
-
-    switch(Words[1]){
-        case "objectives":
-            receiver.items.push(CompletionFunction.createCompletionItem("add", "add", "Adds"));
-            receiver.items.push(CompletionFunction.createCompletionItem("remove", "remove", ""));
-            receiver.items.push(CompletionFunction.createCompletionItem("list", "list", ""));
-            receiver.items.push(CompletionFunction.createCompletionItem("setdisplay", "setdisplay", ""));
-            break;
-
-        case "players":
-            receiver.items.push(CompletionFunction.createCompletionItem("objectives", "objectives", "The handling of objectives for minecraft"));
-            receiver.items.push(CompletionFunction.createCompletionItem("players", "players", "The handling of players, and operations for minecraft"));
-            break;
-    }
+export function CompletionItems(PrefixText: string, position: vscode.Position, receiver: vscode.CompletionList): void {
+    
 }
