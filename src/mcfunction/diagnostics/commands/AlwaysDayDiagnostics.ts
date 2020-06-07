8bdac12ea79ba53c3fe9efde2a3fc7fff29b9f46
DaanV2 <daanverstraten@hotmail.com>
2020-06-07 23:50:23 +0200

    Updated

---
+++ src/mcfunction/diagnostics/commands/AlwaysDayDiagnostics.ts
@@ -0,0 +1,19 @@
+import * as vscode from 'vscode';
+import { DiagnosticsManager,DiagnosticProvider } from '../DiagnosticsManager';
+import { SyntaxItem } from '../../../general/include';
+
+export class AlwaysDayDiagnosticProvider implements DiagnosticProvider {
+
+	//provides diagnostics
+	provideDiagnostic(item: SyntaxItem, lineIndex: number, collector: vscode.Diagnostic[], dm: DiagnosticsManager, document: vscode.TextDocument) : void {
+		var Child = item.Child;
+
+		//[lock: Boolean]
+		if (Child == undefined) {
+			return;
+		}
+
+		dm.BooleanDiagnoser?.provideDiagnostic(Child, lineIndex, collector, dm, document);
+	}
+
+}
