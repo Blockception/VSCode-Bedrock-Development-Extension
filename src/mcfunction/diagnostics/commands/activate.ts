840e1bd6a5854f8f2a6963a15b3b0bdec2cf70a6
DaanV2 <daanverstraten@hotmail.com>
2020-06-08 10:17:47 +0200

    Finished and updated

--- src/mcfunction/diagnostics/commands/activate.ts
+++ src/mcfunction/diagnostics/commands/activate.ts
@@ -2,13 +2,22 @@ import { DiagnosticsManager } from "../DiagnosticsManager";
 
 import { AlwaysDayDiagnosticProvider } from "./alwaysdayDiagnostics";
 import { ClearDiagnosticProvider } from "./clearDiagnostics";
+import { CloneDiagnosticProvider } from "./cloneDiagnostics";
+import { ConnectDiagnosticProvider } from "./connectDiagnostics";
 import { DaylockDiagnosticProvider } from "./DaylockDiagnostics";
+import { DeopDiagnosticProvider } from "./deopDiagnostics";
+import { DifficultyDiagnosticProvider } from "./difficultyDiagnostics";
+import { EffectDiagnosticProvider } from "./effectDiagnostics";
 import { FunctionCommandDiagnosticProvider } from "./FunctionDiagnostic";
 
-export function activate(context: DiagnosticsManager) {    
+export function activate(context: DiagnosticsManager) {
     context.set(new AlwaysDayDiagnosticProvider(), ["alwaysday"]);
     context.set(new ClearDiagnosticProvider(), [ "clear" ]);
+    context.set(new CloneDiagnosticProvider(), ["clone"]);
+    context.set(new ConnectDiagnosticProvider(), ["connect"]);
     context.set(new DaylockDiagnosticProvider(), [ "daylock" ]);
-    context.set(new FunctionCommandDiagnosticProvider(), [ "function" ]);
-    
+    context.set(new DeopDiagnosticProvider(), [ "deop" ]);
+    context.set(new DifficultyDiagnosticProvider(), ["difficulty"]);
+    context.set(new EffectDiagnosticProvider(), ["effect"]);
+    context.set(new FunctionCommandDiagnosticProvider(), [ "function" ]);    
 }
--- src/mcfunction/diagnostics/commands/cloneDiagnostics.ts
+++ src/mcfunction/diagnostics/commands/cloneDiagnostics.ts
@@ -1,29 +1,43 @@
 import * as vscode from 'vscode';
-import { DiagnosticsManager,DiagnosticProvider } from '../DiagnosticsManager';
+import { DiagnosticsManager,DiagnosticProvider, Errors } from '../DiagnosticsManager';
 import { SyntaxItem } from '../../../general/include';
+import { Functions } from '../DiagnosticsFunctions';
 
-export class cloneDiagnosticProvider implements DiagnosticProvider {
+export class CloneDiagnosticProvider implements DiagnosticProvider {
 
 	//provides diagnostics
 	provideDiagnostic(item: SyntaxItem, lineIndex: number, collector: vscode.Diagnostic[], dm: DiagnosticsManager, document: vscode.TextDocument) : void {
+		
+		//begin x y z
+		var out = Functions.provideDiagnosticsXYZ('clone', item, lineIndex, collector, dm, document);
 
-		//<begin: x y z>
-		if (word == undefined) {
-			//MISSING ERROR
+		if (out[1] == false){
+			return;
 		}
 
-		//<end: x y z>
-		if (word == undefined) {
-			//MISSING ERROR
+		//end x y z
+		out = Functions.provideDiagnosticsXYZ('clone <begin x y z>', out[0], lineIndex, collector, dm, document);
+
+		if (out[1] == false){
+			return;
 		}
 
-		//<destination: x y z>
-		if (word == undefined) {
-			//MISSING ERROR
+		//destination x y z
+		out = Functions.provideDiagnosticsXYZ('clone <begin x y z> <end: x y z>', out[0], lineIndex, collector, dm, document);
+
+		if (out[1] == false){
+			return;
+		}
+		
+		var mode = out[0].Child;
+
+		if (mode == undefined){
+			return;
 		}
 
-		switch(item.text) {
-		case '[replace|masked]':
+		switch(mode.Text.text) {
+		case 'masked':
+		case 'replace':
 			this.branchreplace_masked(item, lineIndex, collector, dm, document);
 			return;
 
@@ -32,47 +46,63 @@ export class cloneDiagnosticProvider implements DiagnosticProvider {
 			return;
 
 		default:
-			//NOT FOUND ERROR
+			Errors.UnknownWords('clone <begin x y z> <end: x y z> <destination: x y z>', 'masked, replace, filtered', lineIndex, mode, collector);
 			return;
 		}
-	branchreplace_masked(item: SyntaxItem, lineIndex: number, collector: vscode.Diagnostic[], dm: DiagnosticsManager, document: vscode.TextDocument) : void {
-
-		//[replace|masked]
-		if (word == undefined) {
-			return;
-		}
-
-		//[normal|force|move]
-		if (word == undefined) {
-			return;
-		}
-
 	}
+
+	//Diagnostics when the branch Replace|Masked is taken
+	branchreplace_masked(item: SyntaxItem, lineIndex: number, collector: vscode.Diagnostic[], dm: DiagnosticsManager, document: vscode.TextDocument) : void {
+		var mode = item.Child;
+		if (mode == undefined){
+			return;
+		}
+
+		switch(mode.Text.text) {
+		case 'normal':
+		case 'force':
+		case 'move':
+			return;
+		default:
+			Errors.UnknownWords('clone <begin x y z> <end: x y z> <destination: x y z> [mode]', 'normal, force, move', lineIndex, mode, collector);
+			return;
+		}
+	}
+
+	//Diagnostics when the branch Filtered is taken
 	branchfiltered(item: SyntaxItem, lineIndex: number, collector: vscode.Diagnostic[], dm: DiagnosticsManager, document: vscode.TextDocument) : void {
-
-		//filtered
-		if (word == undefined) {
-			//MISSING ERROR
+		var mode = item.Child;
+		if (mode == undefined){
+			Errors.Missing('clone mode', 'clone <begin x y z> <end: x y z> <destination: x y z> filtered', lineIndex, item, collector);
+			return;
 		}
 
-		//<normal|force|move>
-		if (word == undefined) {
-			//MISSING ERROR
+		switch(mode.Text.text) {
+		case 'normal':
+		case 'force':
+		case 'move':
+			break;
+		default:
+			Errors.UnknownWords('clone <begin x y z> <end: x y z> <destination: x y z> [mode]', 'normal, force, move', lineIndex, mode, collector);
+			return;
 		}
+
+		var block = mode.Child;
 
 		//<tileName: Block>
-		if (word == undefined) {
-			//MISSING ERROR
+		if (block == undefined) {
+			Errors.Missing('block', 'clone <begin x y z> <end: x y z> <destination: x y z> filtered <clone mode>', lineIndex, mode, collector);
+			return;
 		}
-		dm.BlockDiagnoser?.provideDiagnostic(word, lineIndex, collector, dm, document);
+		dm.BlockDiagnoser?.provideDiagnostic(block, lineIndex, collector, dm, document);
+
+		var blockData = block.Child;
 
 		//<tileData: int>
-		if (word == undefined) {
-			//MISSING ERROR
+		if (blockData == undefined) {
+			Errors.Missing('integer', 'clone <begin x y z> <end: x y z> <destination: x y z> filtered <clone mode> <block>', lineIndex, block, collector);
+			return;
 		}
-		dm.IntegerDiagnoser?.provideDiagnostic(word, lineIndex, collector, dm, document);
-
+		dm.IntegerDiagnoser?.provideDiagnostic(blockData, lineIndex, collector, dm, document);
 	}
-	}
-
 }
