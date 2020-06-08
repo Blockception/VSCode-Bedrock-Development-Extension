8f0dac5978449bdc028a9e25129a12d9ca8eb63e
DaanV2 <daanverstraten@hotmail.com>
2020-06-08 10:55:26 +0200

    Finished enchant to gamerule

--- src/mcfunction/diagnostics/commands/activate.ts
+++ src/mcfunction/diagnostics/commands/activate.ts
@@ -1,14 +1,19 @@
 import { DiagnosticsManager } from "../DiagnosticsManager";
 
-import { AlwaysDayDiagnosticProvider } from "./alwaysdayDiagnostics";
-import { ClearDiagnosticProvider } from "./clearDiagnostics";
-import { CloneDiagnosticProvider } from "./cloneDiagnostics";
-import { ConnectDiagnosticProvider } from "./connectDiagnostics";
+import { AlwaysDayDiagnosticProvider } from "./AlwaysdayDiagnostics";
+import { ClearDiagnosticProvider } from "./ClearDiagnostics";
+import { CloneDiagnosticProvider } from "./CloneDiagnostics";
+import { ConnectDiagnosticProvider } from "./ConnectDiagnostics";
 import { DaylockDiagnosticProvider } from "./DaylockDiagnostics";
-import { DeopDiagnosticProvider } from "./deopDiagnostics";
-import { DifficultyDiagnosticProvider } from "./difficultyDiagnostics";
-import { EffectDiagnosticProvider } from "./effectDiagnostics";
+import { DeopDiagnosticProvider } from "./DeopDiagnostics";
+import { DifficultyDiagnosticProvider } from "./DifficultyDiagnostics";
+import { EffectDiagnosticProvider } from "./EffectDiagnostics";
+import { EnchantDiagnosticProvider } from "./EnchantDiagnostics";
+import { ExecuteDiagnosticProvider } from "./ExecuteDiagnostic";
+import { FillDiagnosticProvider } from "./FillDiagnostics";
 import { FunctionCommandDiagnosticProvider } from "./FunctionDiagnostic";
+import { GamemodeDiagnosticProvider } from "./GamemodeDiagnostics";
+import { GameruleDiagnosticProvider } from "./GameruleDiagnostics";
 
 export function activate(context: DiagnosticsManager) {
     context.set(new AlwaysDayDiagnosticProvider(), ["alwaysday"]);
@@ -19,5 +24,10 @@ export function activate(context: DiagnosticsManager) {
     context.set(new DeopDiagnosticProvider(), [ "deop" ]);
     context.set(new DifficultyDiagnosticProvider(), ["difficulty"]);
     context.set(new EffectDiagnosticProvider(), ["effect"]);
-    context.set(new FunctionCommandDiagnosticProvider(), [ "function" ]);    
+    context.set(new EnchantDiagnosticProvider(), ["effect"]);
+    context.set(new ExecuteDiagnosticProvider(), ["execute"]);
+    context.set(new FillDiagnosticProvider(), ["fill"]);
+    context.set(new FunctionCommandDiagnosticProvider(), [ "function" ]);
+    context.set(new GamemodeDiagnosticProvider(), [ "gamemode" ]);
+    context.set(new GameruleDiagnosticProvider(), [ "gamerule" ]);
 }
