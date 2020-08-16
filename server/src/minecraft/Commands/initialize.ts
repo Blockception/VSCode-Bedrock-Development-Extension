83f526060b926c76441a293f1ed45115a5d4b4aa
DaanV2 <daanverstraten@hotmail.com>
2020-08-16 14:10:42 +0200

    Updated

--- server/src/minecraft/Commands/initialize.ts
+++ server/src/minecraft/Commands/initialize.ts
@@ -30,7 +30,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 import { AddCommand } from './ToCommand';
 
 export function AddCommands() : void {
-
 	AddCommand("alwaysday [lock:boolean]");
 
 	AddCommand("clear [player:selector] [itemName:item] [data:int] [maxCount:int]");
