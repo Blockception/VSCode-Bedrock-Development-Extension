de1bae59ac93f803580169a0fb7a9b98f6a4bbbd
DaanV2 <daanverstraten@hotmail.com>
2020-10-17 15:27:09 +0200

    Renamed ObjectBase -> Object base

--- server/src/minecraft/types/Sound/Sound.ts
+++ server/src/minecraft/types/Sound/Sound.ts
@@ -29,7 +29,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 import { Location, Range } from "vscode-languageserver";
 import { Identifiable } from '../../Interfaces/Identifiable';
-import { ObjectBase } from '../ObjectBase/ObjectBase';
+import { ObjectBase } from '../Object Base/ObjectBase';
 
 export class Sound extends ObjectBase {
   
