8dc9ff15e90532473af7dc3ff0b4434f61432087
DaanV2 <daanverstraten@hotmail.com>
2020-10-26 12:53:51 +0100

    Updated coordinate completion to prefere '~'

--- server/src/minecraft/types/Coordinate/Completion.ts
+++ server/src/minecraft/types/Coordinate/Completion.ts
@@ -34,9 +34,11 @@ export function provideCoordinateCompletion(receiver: CompletionList): void {
   const Kind = Kinds.Completion.Coordinate;
 
   receiver.items.push(
+    { label: "~", documentation: "Relative coordinate", kind: Kind, preselect:true },
     { label: "~1", documentation: "Relative coordinate", kind: Kind },
     { label: "~-1", documentation: "Relative coordinate", kind: Kind },
     { label: "^1", documentation: "Local coordinate", kind: Kind },
+    { label: "^", documentation: "Local coordinate", kind: Kind },
     { label: "^-1", documentation: "Local coordinate", kind: Kind },
     { label: "1", documentation: "Coordinate", kind: Kind },
     { label: "-1", documentation: "Coordinate", kind: Kind }
