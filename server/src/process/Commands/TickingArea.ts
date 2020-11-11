3f87250c1bfac687799d534d27d4933646a9e3b7
DaanV2 <daanverstraten@hotmail.com>
2020-11-11 18:49:56 +0100

    Updated tickingarea parsing

--- server/src/process/Commands/TickingArea.ts
+++ server/src/process/Commands/TickingArea.ts
@@ -7,15 +7,15 @@ Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
 1. Redistributions of source code must retain the above copyright notice, this
-	 list of conditions and the following disclaimer.
+   list of conditions and the following disclaimer.
 
 2. Redistributions in binary form must reproduce the above copyright notice,
-	 this list of conditions and the following disclaimer in the documentation
-	 and/or other materials provided with the distribution.
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
 
 3. Neither the name of the copyright holder nor the names of its
-	 contributors may be used to endorse or promote products derived from
-	 this software without specific prior written permission.
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -33,12 +33,13 @@ import { Database } from "../../database/include";
 import { CommandIntr } from "../../minecraft/commands/include";
 import { Tickingarea } from "../../minecraft/types/Tickingarea/include";
 
-export function ProcessTickingAreaCommand(line: string, lineIndex: number, doc: TextDocument): void {
-  if (!line.startsWith("tickingarea add")) return;
+export function ProcessTickingAreaCommand(Command: CommandIntr, lineIndex: number, doc: TextDocument): void {
+  //tickingarea add 
+  if (Command.Paramaters[1]?.text !== 'add')
+    return;
 
-  let Command = CommandIntr.parse(line, { character: 0, line: lineIndex }, doc.uri);
-
-  if (line.startsWith("tickingarea add circle")) {
+  //tickingarea add circle
+  if (Command.Paramaters[2]?.text === 'circle') {
     ProcessCircleCommand(Command);
   } else {
     ProcessBoxCommand(Command);
