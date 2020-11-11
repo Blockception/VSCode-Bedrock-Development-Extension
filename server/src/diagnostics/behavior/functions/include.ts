900135947361883f9f406833a211ccd8984cbad0
DaanV2 <daanverstraten@hotmail.com>
2020-11-11 18:16:25 +0100

    auto: Regenerated

--- server/src/diagnostics/behavior/functions/include.ts
+++ server/src/diagnostics/behavior/functions/include.ts
@@ -1,2 +1,3 @@
 /*	Auto generated	*/
+export * as parameters from './parameters/include';
 export * from "./Diagnostics";
---
+++ server/src/diagnostics/behavior/functions/parameters/include.ts
@@ -0,0 +1,7 @@
+/*	Auto generated	*/
+export * from "./gamemode";
+export * from "./keyword";
+export * from "./objective";
+export * from "./particle";
+export * from "./tag";
+export * from "./tickingarea";
