a6f63a7cbc3da4927961b17e560ce41c8e0802a1
DaanV2 <daanverstraten@hotmail.com>
2020-11-20 10:17:14 +0100

    Updated json Item

--- server/src/types/minecraft/json/ItemComponents/Constants.ts
+++ server/src/types/minecraft/json/ItemComponents/Constants.ts
@@ -1,7 +1,6 @@
 export const CanDestroyComponent = '"minecraft:can_destroy": { "blocks": [ ] }';
 export const CanPlaceOnComponent = '"minecraft:can_place_on": { "blocks": [ ] }';
-export const LockInInventoryComponent = '"minecraft:lock_in_inventory":{}';
-export const KeepOnDeathComponent = '"minecraft:keep_on_death":{}';
-export const LockInSlotComponent = '"minecraft:lock_in_slot":{}';
-export const Example =
-  '{ "minecraft:can_destroy": { "blocks": [ "grass" ] }, "minecraft:can_place_on": { "blocks": [ "grass" ] }, "minecraft:lock_in_inventory":{}, "minecraft:keep_on_death":{}, "minecraft:lock_in_slot":{} }';
+export const LockInInventoryComponent = '"item_lock": { "mode": "lock_in_inventory" }';
+export const KeepOnDeathComponent = '"keep_on_death": {}';
+export const LockInSlotComponent = '"item_lock": { "mode": "lock_in_slot" } ';
+export const Example = `{ ${CanDestroyComponent}, ${CanPlaceOnComponent}, ${LockInInventoryComponent}, ${LockInSlotComponent}, ${KeepOnDeathComponent} }`;
