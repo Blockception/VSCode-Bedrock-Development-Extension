47907676a78ae78500462bb51127328ed0bcc198
DaanV2 <daanverstraten@hotmail.com>
2021-01-01 13:52:29 +0100

    Updated

--- server/src/semantics/OnRequest.ts
+++ server/src/semantics/OnRequest.ts
@@ -48,8 +48,9 @@ export function OnProvideRangeSemanticRequestAsync(params: SemanticTokensRangePa
 
 function OnProvideSemanticRequest(params: SemanticTokensRangeParams | SemanticTokensParams): SemanticTokens {
   if (params.textDocument.uri.includes("vscode://schemas")) return { data: [] };
+  if (params.textDocument.uri.includes("git:/")) return { data: [] };
 
-  console.log(params.textDocument.uri);
+  //console.log(params.textDocument.uri);
 
   let doc = GetDocument(params.textDocument.uri);
   console.log("Semantic tokens: " + GetFilename(doc.uri) + " | " + doc.languageId);
