182e8242661c21ec01096eb72ac8b2c9da45268c
DaanV2 <daanverstraten@hotmail.com>
2021-04-10 21:12:04 +0200

    Updated

--- server/src/Lib/Types/Minecraft/Behavior/Entities/Entity.ts
+++ server/src/Lib/Types/Minecraft/Behavior/Entities/Entity.ts
@@ -39,6 +39,28 @@ export interface ComponentContainer {
   [component: string]: any;
 }
 
+export namespace ComponentContainer {
+  export function MatchComponentName(data: ComponentContainer | undefined, match: string): boolean {
+    if (data) {
+      for (const component in data) {
+        if (component.includes(match)) return true;
+      }
+    }
+
+    return false;
+  }
+
+  export function HasComponentName(data: ComponentContainer | undefined, match: string): boolean {
+    if (data) {
+      for (const component in data) {
+        if (component === match) return true;
+      }
+    }
+
+    return false;
+  }
+}
+
 export interface Entity {
   format_version: string;
   "minecraft:entity": {
@@ -69,6 +91,30 @@ export namespace Entity {
 
       if (mce && mce.description && mce.description.identifier) {
         return true;
+      }
+    }
+
+    return false;
+  }
+
+  export function MatchComponentName(data: Entity, match: string): boolean {
+    if (ComponentContainer.MatchComponentName(data["minecraft:entity"].components, match)) return true;
+
+    if (data["minecraft:entity"].component_groups) {
+      for (const group in data["minecraft:entity"].component_groups) {
+        if (ComponentContainer.MatchComponentName(data["minecraft:entity"].component_groups[group], match)) return true;
+      }
+    }
+
+    return false;
+  }
+
+  export function HasComponentName(data: Entity, match: string): boolean {
+    if (ComponentContainer.HasComponentName(data["minecraft:entity"].components, match)) return true;
+
+    if (data["minecraft:entity"].component_groups) {
+      for (const group in data["minecraft:entity"].component_groups) {
+        if (ComponentContainer.HasComponentName(data["minecraft:entity"].component_groups[group], match)) return true;
       }
     }
 
