7c03b026888132973a2c43deacf9d5e6b469cb0e
DaanV2 <daanverstraten@hotmail.com>
2021-04-11 13:50:50 +0200

    Updated ride file mode

--- server/src/Lib/Types/Commands/Modes/RideFillMode/RideFillMode.ts
+++ server/src/Lib/Types/Commands/Modes/RideFillMode/RideFillMode.ts
@@ -3,10 +3,7 @@ import { ModeCollection } from "../Interface";
 export const RideFillMode: ModeCollection = {
   Name: "Ride Fill Mode",
   Modes: [
-    { Name: "destroy", Description: "Destroy the old block, if tile drops is on, they all will drop an item" },
-    { Name: "hollow", Description: "Create a hollow cube" },
-    { Name: "keep", Description: "Keeps the old blocks, replaces only air" },
-    { Name: "outline", Description: "Just sets the outline" },
-    { Name: "replace", Description: "Replaces a specified block with the given block" },
+    { Name: "if_group_fits", Description: "If the entity fits then its placed" },
+    { Name: "until_full", Description: "Keep adding entities until full" },
   ],
 };
