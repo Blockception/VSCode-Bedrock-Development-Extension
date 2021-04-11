892d5e4631480fa5a8d763a5975954f91b6d149e
DaanV2 <daanverstraten@hotmail.com>
2021-04-11 12:28:38 +0200

    Updated

--- server/src/Lib/Types/Commands/Modes/include.ts
+++ server/src/Lib/Types/Commands/Modes/include.ts
@@ -1,6 +1,8 @@
 /*	Auto generated	*/
+export * as CameraShake from "./CameraShakeMode/include";
+export * as Clone from "./CloneMode/include";
 export * as OldBlockMode from "./OldBlockMode/include";
-export * as Operation from "./Operation/include";
+export * as Operation from "./OperationMode/include";
 export * from "./Completion";
 export * from "./Diagnose";
 export * from "./Functions";
