5a0211992ebbf1af269765f048398dd9291eb624
DaanV2 <daanverstraten@hotmail.com>
2021-04-11 13:38:14 +0200

    Added Teleport rules mode

---
+++ server/src/Lib/Types/Commands/Modes/TeleportRulesMode/Diagnose.ts
@@ -0,0 +1,12 @@
+import { LocationWord } from "bc-vscode-words";
+import { CommandIntr } from "../../Interpertation/include";
+import { DiagnoseMode } from "../Diagnose";
+import { DiagnosticsBuilder } from "../../../../Diagnostics/Builder";
+import { TeleportRulesMode } from "./TeleportRulesMode";
+
+export function Diagnose(Word: LocationWord, Command: CommandIntr, builder: DiagnosticsBuilder): void {
+  DiagnoseMode(Word, TeleportRulesMode, builder);
+  let Index = Command.Parameters.indexOf(Word);
+
+  if (Index < 0) return;
+}
