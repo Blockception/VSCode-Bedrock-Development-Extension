a285ea0615ff248fd34e711bb4ae4a26b8b900f2
DaanV2 <daanverstraten@hotmail.com>
2021-05-07 15:08:03 +0200

    Removed

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -2,12 +2,12 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-import { isMainThread } from "worker_threads";
+//import { isMainThread } from "worker_threads";
 import { SetupServer } from "./Lib/Server/include";
 
 //Setup the server
-if (isMainThread) {
-  SetupServer();
-} else {
-  //If worker thread
-}
+//if (isMainThread) {
+SetupServer();
+//} else {
+//If worker thread
+//}
