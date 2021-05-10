6cfa72f31ec8ce19066cf886a30d08a7e6f57577
DaanV2 <daanverstraten@hotmail.com>
2021-05-10 10:39:21 +0200

    Updated references

--- server/src/Lib/Code/include.ts
+++ server/src/Lib/Code/include.ts
@@ -1,11 +1,10 @@
 /*	Auto generated	*/
-export * as Json from "./Json/include";
 export * from "./Array";
 export * from "./Character";
 export * from "../Types/Document/Document";
 export * from "./Equal";
 export * from "./File";
-export * from "./ProjectData";
+export * from "./ProjectFiles";
 export * from "./Range";
 export * from "./Reader";
 export * from "./TextEdit";
