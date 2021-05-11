77406a165447edf661ac0f7d5187d3741881eb0c
DaanV2 <daanverstraten@hotmail.com>
2021-05-11 10:08:17 +0200

    auto: Updated dependecies

--- scripts/update.ps1
+++ scripts/update.ps1
@@ -1,9 +1,16 @@
-npm update
 npm install -g npm-check-updates
-npm install
+
+npm update
 npm audit fix
 
+cd client
+npm update
+npm audit fix
 
+cd ../server
+npm update
+npm audit fix
 
+cd ..
 git add .
 git commit -m "auto: Updated dependecies"
