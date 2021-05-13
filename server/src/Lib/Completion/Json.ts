0bdee9a3aa18042c3550e7a3d79e7f4ca92b93a3
DaanV2 <daanverstraten@hotmail.com>
2021-05-13 19:44:50 +0200

    Refactor json completion

--- server/src/Lib/Completion/Json.ts
+++ server/src/Lib/Completion/Json.ts
@@ -2,6 +2,7 @@ import { CompletionItem, InsertReplaceEdit, Range } from "vscode-languageserver"
 import { GetCurrentString } from "../Types/Document/Json Functions";
 import { TextDocument } from "../Types/Document/TextDocument";
 import { DetectGeneralDataType, GeneralDataType } from "../Types/Minecraft/Format/include";
+import { Resource } from "../Types/Minecraft/include";
 import { CompletionBuilder } from "./Builder";
 import { OnCompletionMcFunctionLine } from "./Mcfunction";
 import { OnCompletionEntityEvents, OnCompletionMolang } from "./Molang/Molang";
@@ -11,24 +12,39 @@ export function OnCompletionJson(doc: TextDocument, cursor: number, receiver: Co
 
   if (type == GeneralDataType.unknown) return;
 
-  let text = doc.getText();
-  let range = GetCurrentString(text, cursor);
+  OnCompletionJsonMolang(doc, cursor, receiver);
+
+  switch (type) {
+    case GeneralDataType.resource_pack:
+      return Resource.ProvideCompletion(doc, cursor, receiver);
+
+    default:
+      break;
+  }
+}
+
+function OnCompletionJsonMolang(doc: TextDocument, cursor: number, receiver: CompletionBuilder) {
+  const text = doc.getText();
+  const range = GetCurrentString(text, cursor);
 
   //If start has not been found or not a property
   if (range == undefined) return;
 
-  let data = text.substring(range.start, range.end);
-  let insertIndex = cursor - range.start;
-  let first = '"' + data.substring(0, insertIndex);
-  let second = data.substring(insertIndex) + '"';
-  let P = doc.positionAt(cursor);
-  let R = Range.create(P, P);
+  const data = text.substring(range.start, range.end);
+  const insertIndex = cursor - range.start;
+  const first = '"' + data.substring(0, insertIndex);
+  const second = data.substring(insertIndex) + '"';
+  const P = doc.positionAt(cursor);
+  const R = Range.create(P, P);
 
   //Have each new item pass through a new function
+  var Function = receiver.OnNewItem;
   receiver.OnNewItem = (NewItem: CompletionItem) => {
     //Update the filtering text
     NewItem.filterText = first + NewItem.label + second;
     NewItem.textEdit = InsertReplaceEdit.create(NewItem.label, R, R);
+
+    if (Function) Function(NewItem);
   };
 
   //Find all events
@@ -41,5 +57,5 @@ export function OnCompletionJson(doc: TextDocument, cursor: number, receiver: Co
     OnCompletionMolang(data, cursor - range.start, doc, receiver);
   }
 
-  receiver.OnNewItem = undefined;
+  receiver.OnNewItem = Function;
 }
