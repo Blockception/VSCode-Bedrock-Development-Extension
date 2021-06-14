7edb5b57a9aa530ed4b49696c5cadd401fae8906
DaanV2 <daanverstraten@hotmail.com>
2021-06-14 12:07:34 +0200

    Updated

--- server/src/Lib/Types/Minecraft/Resource/Function.test.ts
+++ server/src/Lib/Types/Minecraft/Resource/Function.test.ts
@@ -5,6 +5,6 @@ describe("Resourcepack Functions", () => {
   it("GetResourcePack", () => {
     const path = GetResourcePack("file:///f%3A/Projects/Org%20B/Project-Foo/1.%20Resource%20Pack-RP/sounds/sound_definitions.json", "sounds");
 
-    expect(path).to.equal("f%3A/Projects/Org%20B/Project-Foo/1.%20Resource%20Pack-RP");
+    expect(path).to.equal("f:/Projects/Org%20B/Project-Foo/1.%20Resource%20Pack-RP");
   });
 });
