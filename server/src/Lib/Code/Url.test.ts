24018dbe60b8d0b65d0730f6b17005626dd0a015
DaanV2 <daanverstraten@hotmail.com>
2021-08-16 12:04:30 +0200

    Updating GetFilepath test

--- server/src/Lib/Code/Url.test.ts
+++ server/src/Lib/Code/Url.test.ts
@@ -15,4 +15,8 @@ describe("Url", () => {
   it("Uri To Filepath", () => {
     expect(GetFilepath("file:///f%3A/temp%20folder/something.json")).to.equal("f:/temp folder/something.json");
   });
+
+  it("Uri To Filepath2", () => {
+    expect(GetFilepath("///f%3A/temp%20folder/something.json")).to.equal("f:/temp folder/something.json");
+  });
 });
