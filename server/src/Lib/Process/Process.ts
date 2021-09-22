0ce13da275261b81a4fba529ee3ffe88859cbde4
DaanV2 <daanverstraten@hotmail.com>
2021-09-22 17:49:14 +0200

    Cleaned

--- server/src/Lib/Process/Process.ts
+++ server/src/Lib/Process/Process.ts
@@ -2,12 +2,6 @@ import { TextDocument } from "../Types/Document/TextDocument";
 import { Database } from "../Database/include";
 import { HandleError } from "../Code/Error";
 import { ProvideDiagnostics } from "../Diagnostics/OnRequest";
-import { Pack, BehaviorPack, ResourcePack } from "bc-minecraft-bedrock-project";
-import { Fs } from "../Code/Url";
-import { MinecraftFormat } from "../Minecraft/Format";
-import { Document } from "../Types/include";
-import { Console } from "../Manager/Console";
-
 //Process the given document
 export function Process(document: TextDocument): void {
   //Console.Log("Processing: " + GetFilename(document.uri) + " | " + document.languageId);
