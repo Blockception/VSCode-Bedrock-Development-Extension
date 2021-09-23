9127df7de44b031e15391ec3bdaa8850e723a02a
DaanV2 <daanverstraten@hotmail.com>
2021-09-23 11:06:19 +0200

    Updated

--- server/src/Lib/Commands/Diagnose Project.ts
+++ server/src/Lib/Commands/Diagnose Project.ts
@@ -1,11 +1,9 @@
-import { Pack } from "bc-minecraft-bedrock-project";
 import { ExecuteCommandParams } from "vscode-languageserver";
-import { ProvidePackDiagnostics } from "../Diagnostics/OnRequest";
 import { Console } from "../Manager/Console";
 import { Traverse } from "../Process/Traverse";
 
 export function DiagnoseProjectCommand(params: ExecuteCommandParams) {
   Console.Log("Starting on diagnosing project");
 
-  Traverse().then((packs) => packs.forEach(ProvidePackDiagnostics));
+  Traverse();
 }
--- server/src/Lib/Process/Traverse.ts
+++ server/src/Lib/Process/Traverse.ts
@@ -1,5 +1,6 @@
 import { Pack } from "bc-minecraft-bedrock-project";
 import { HandleError } from "../Code/Error";
+import { ProvidePackDiagnostics } from "../Diagnostics/OnRequest";
 import { Console } from "../Manager/Console";
 import { Manager } from "../Manager/Manager";
 import { Workspace } from "../Workspace/Workspace";
@@ -20,6 +21,8 @@ export async function Traverse(): Promise<Pack[]> {
   out.catch((err) => {
     HandleError(err);
   });
+
+  out.then((packs) => packs.forEach(ProvidePackDiagnostics));
 
   return out;
 }
