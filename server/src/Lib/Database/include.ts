56cd4a02503b9591eaca839530b35dee919bc7cd
DaanV2 <daanverstraten@hotmail.com>
2021-09-26 23:52:53 +0200

    Refactor

--- server/src/Lib/Database/include.ts
+++ server/src/Lib/Database/include.ts
@@ -1,3 +1,2 @@
 /*	Auto generated	*/
-export * as Types from "./Types/include";
 export * from "./Database";
--- server/src/Lib/Definition/include.ts
+++ server/src/Lib/Definition/include.ts
@@ -1,6 +1,3 @@
 /*	Auto generated	*/
 
-export * from "./Json";
-export * from "./Mcfunction";
 export * from "./OnRequest";
-export * from "./Search";
