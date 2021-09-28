659753807a7d9695578c3ec90960f4a9a1cf33fb
DaanV2 <daanverstraten@hotmail.com>
2021-09-28 16:03:22 +0200

    Updated Code Action Builder

--- server/src/Lib/CodeAction/Builder.ts
+++ server/src/Lib/CodeAction/Builder.ts
@@ -1,4 +1,5 @@
-import { CodeAction, CodeActionParams, Command } from "vscode-languageserver";
+import { CodeAction, CodeActionParams, Command, Range } from "vscode-languageserver";
+import { GetDocument } from "../Types/Document/include";
 
 /** */
 export class CodeActionBuilder {
@@ -13,11 +14,52 @@ export class CodeActionBuilder {
     this.out = [];
   }
 
+  /**
+   * 
+   * @returns 
+   */
+  getText(range : Range | undefined): string {
+    const doc = GetDocument(this.params.textDocument.uri);
+    if (!doc) return "";
+    return doc.getText(range ?? this.params.range);
+  }
+
   /** */
   Push(item: Command | CodeAction | undefined): Command | CodeAction | undefined {
     if (item) {
       this.out.push(item);
     }
+
+    return item;
+  }
+
+  /**
+   *
+   * @param title
+   * @param commandid
+   * @param args
+   * @returns
+   */
+
+  Command(title: string, commandid: string, args: string[] | undefined): Command {
+    const item: Command = { command: commandid, title: title, arguments: args };
+
+    this.out.push(CodeAction.create(title, item, "create"));
+
+    return item;
+  }
+
+  /**
+   *
+   * @param title
+   * @returns
+   */
+  Action(title: string): CodeAction {
+    const item: CodeAction = {
+      title: title,
+    };
+
+    this.out.push(item);
 
     return item;
   }
