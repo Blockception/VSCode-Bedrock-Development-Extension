55399e6d84f72d2fdaf18b5e52a34fc77f20a15d
DaanV2 <daanverstraten@hotmail.com>
2021-10-01 12:12:48 +0200

    Added loot tables and trading tables

--- server/src/Lib/Minecraft/BehaviorPack/Completion.ts
+++ server/src/Lib/Minecraft/BehaviorPack/Completion.ts
@@ -1,6 +1,10 @@
-import { SimpleContext } from "../../Code/SimpleContext";
-import { CompletionBuilder } from "../../Completion/include";
+import { JsonCompletionContext } from "../../Completion/include";
+import { LootTables, Trading } from "./include";
 
-export function ProvideCompletion(context: SimpleContext<CompletionBuilder>) {
-  //TODO loot_tables, trading
+export function ProvideCompletion(context: JsonCompletionContext) {
+  //Prepare data to be fixed for json
+  const data = context.currentText;
+
+  if (data.startsWith("loot_tables")) LootTables.ProvideCompletion(context);
+  if (data.startsWith("trading")) Trading.ProvideCompletion(context);
 }
