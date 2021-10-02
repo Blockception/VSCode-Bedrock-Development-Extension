613516645aefa6238068fd9de664d14b193e172d
DaanV2 <daanverstraten@hotmail.com>
2021-10-02 19:42:45 +0200

    Added

---
+++ server/src/Lib/Minecraft/Molang/Hover.ts
@@ -0,0 +1,6 @@
+import { TextDocument } from 'bc-minecraft-bedrock-project';
+import { HoverParams } from 'vscode-languageserver-protocol';
+
+export function ProvideHover(doc : TextDocument, params : HoverParams) {
+	
+}
