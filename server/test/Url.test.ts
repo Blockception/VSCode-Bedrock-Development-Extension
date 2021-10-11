1f40abe6e23f44f6ac24f0d981eb748c990332c0
DaanV2 <daanverstraten@hotmail.com>
2021-10-11 18:56:35 +0200

    Rewriting tests

--- server/test/Url.test.ts
+++ server/test/Url.test.ts
@@ -1,16 +1,13 @@
 import { expect } from "chai";
 import * as fs from "fs";
 import path from "path";
-import { URI } from 'vscode-uri';
+import { URI } from "vscode-uri";
 import { Fs, Vscode } from "../src/Lib/Code/Url";
 
 describe("URL", () => {
   describe("Fs", () => {
     describe("folders", () => {
-      const folders = [
-        path.resolve(__dirname, "files"), 
-        Fs.FromVscode(Vscode.FromFs(path.resolve(__dirname, "files")))
-      ];
+      const folders = [path.resolve(__dirname, "files"), Fs.FromVscode(Vscode.FromFs(path.resolve(__dirname, "files")))];
 
       for (let I = 0; I < folders.length; I++) {
         const f = folders[I];
@@ -22,7 +19,7 @@ describe("URL", () => {
       const files = [
         path.resolve(__dirname, "files", "example.entity.json"),
         Fs.FromVscode(Vscode.FromFs(path.resolve(__dirname, "files", "example.entity.json"))),
-        Fs.FromVscode(URI.file(path.resolve(__dirname, "files", "example.entity.json")).path)
+        Fs.FromVscode(URI.file(path.resolve(__dirname, "files", "example.entity.json")).path),
       ];
 
       for (let I = 0; I < files.length; I++) {
@@ -35,7 +32,7 @@ describe("URL", () => {
 
 function CheckFolder(folder: string, result: boolean) {
   if (result) {
-    console.log("checking folder exists: " + folder);
+    //console.log("checking folder exists: " + folder);
     expect(fs.existsSync(folder), folder).to.be.true;
     expect(fs.lstatSync(folder).isFile()).to.be.false;
   } else {
@@ -45,7 +42,7 @@ function CheckFolder(folder: string, result: boolean) {
 
 function CheckFile(file: string, result: boolean) {
   if (result) {
-    console.log("checking file exists: " + file);
+    //console.log("checking file exists: " + file);
     expect(fs.existsSync(file), file).to.be.true;
     expect(fs.lstatSync(file).isFile()).to.be.true;
   } else {
