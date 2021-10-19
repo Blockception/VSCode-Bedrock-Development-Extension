5eb3a1ff00e37c10c871227c9128903cffa7e074
DaanV2 <daanverstraten@hotmail.com>
2021-10-19 10:36:05 +0200

    Extended geomtry completion

--- server/src/Lib/Minecraft/Molang/Geometry/Completion.ts
+++ server/src/Lib/Minecraft/Molang/Geometry/Completion.ts
@@ -1,28 +1,66 @@
 import { PackType } from "bc-minecraft-bedrock-project";
-import { Defined, MolangData } from "bc-minecraft-molang";
-import { CompletionItemKind } from "vscode-languageserver-types";
+import { ResourcePack, BehaviorPack } from "bc-minecraft-bedrock-project";
+import { Vanilla } from "bc-minecraft-bedrock-vanilla-data";
 import { SimpleContext } from "../../../Code/SimpleContext";
 import { CompletionBuilder } from "../../../Completion/Builder";
 import { Database } from "../../../Database/include";
+import { Kinds } from "../../General/Kinds";
 
 export function ProvideCompletion(context: SimpleContext<CompletionBuilder>): void {
   const packType = PackType.detect(context.doc.uri);
 
   switch (packType) {
     case PackType.behavior_pack:
-      return;
+      return ProvideBehavioroackCompletion(context);
 
     case PackType.resource_pack:
-      Database.ProjectData.ResourcePacks.entities.forEach((entity) => GenerateDU(entity.molang.geometries, context.receiver, entity.id));
+      return ProvideResourcepackCompletion(context);
   }
 }
 
-function Generate(data: MolangData.Data, builder: CompletionBuilder, kinds: CompletionItemKind = CompletionItemKind.Reference): void {
-  builder.Add(data.id, data.documentation ?? `The molang geometry: ${data.id}`, kinds);
+export function ProvideResourcepackCompletion(context: SimpleContext<CompletionBuilder>): void {
+  const fileType = ResourcePack.FileType.detect(context.doc.uri);
+  const receiver = context.receiver;
+  const kind = Kinds.Completion.Models;
+
+  switch (fileType) {
+    case ResourcePack.FileType.item:
+    case ResourcePack.FileType.entity:
+    case ResourcePack.FileType.attachable:
+      //Using model geometries
+      const gen = (item: ResourcePack.Model.Model) => `The geomtry: ${item}\nDeclared in: ${item.location.uri}`;
+      receiver.Generate(Database.ProjectData.ResourcePacks.models, gen, kind);
+
+      receiver.GenerateStr(Vanilla.ResourcePack.Models, (item) => `The vanilla geometry: ${item}`, kind);
+      break;
+
+    default:
+    case ResourcePack.FileType.animation:
+    case ResourcePack.FileType.animation_controller:
+    case ResourcePack.FileType.render_controller:
+      //Using defined geometries
+      Database.ProjectData.ResourcePacks.entities.forEach((entity) => {
+        const gen = (item: string) => `The defined geomtry: ${item}\nDeclared by: ${entity.id}`;
+        receiver.GenerateStr(entity.molang.geometries.defined, gen, kind);
+      });
+      break;
+  }
 }
 
-function GenerateDU(data: Defined<string>, builder: CompletionBuilder, ownerid: string, kinds: CompletionItemKind = CompletionItemKind.Reference): void {
-  data.defined.forEach((item) => {
-    builder.Add(item, `The molang geometry: ${item}\nDeclared by '${ownerid}'`, kinds);
-  });
+export function ProvideBehavioroackCompletion(context: SimpleContext<CompletionBuilder>): void {
+  const fileType = BehaviorPack.FileType.detect(context.doc.uri);
+  const receiver = context.receiver;
+  const kind = Kinds.Completion.Models;
+
+  switch (fileType) {
+    case BehaviorPack.FileType.block:
+    case BehaviorPack.FileType.item:
+    case BehaviorPack.FileType.entity:
+      //Using model geometries
+      const gen = (item: ResourcePack.Model.Model) => `The geomtry: ${item}\nDeclared in: ${item.location.uri}`;
+      receiver.Generate(Database.ProjectData.ResourcePacks.models, gen, kind);
+
+      receiver.GenerateStr(Vanilla.ResourcePack.Models, (item) => `The vanilla geometry: ${item}`, kind);
+      break;
+  }
 }
