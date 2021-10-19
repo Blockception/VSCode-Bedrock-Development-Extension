53326147eef38c15ed5f1f6238fb44f343bcebf6
DaanV2 <daanverstraten@hotmail.com>
2021-10-19 17:09:00 +0200

    Removing tasks

--- client/src/Minecraft/Manifests/Functions.ts
+++
@@ -1,7 +0,0 @@
-import * as vscode from "vscode";
-import { Manifest } from 'bc-minecraft-bedrock-project/lib/src/Lib/Internal/include';
-import { PackType } from 'bc-minecraft-bedrock-project/lib/src/Lib/Project/Enum/PackType';
-
-export function ManifestType(uri :string) : PackType {
-	const Type = Manifest.DetectTypeUri(uri, (uri)=>vscode.workspace.openTextDocument(uri) );
-}
