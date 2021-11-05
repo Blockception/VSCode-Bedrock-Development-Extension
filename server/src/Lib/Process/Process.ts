cfa9435fcc1c9b1efb15af1e042967f8cf6991cf
DaanV2 <daanverstraten@hotmail.com>
2021-11-05 11:36:53 +0100

    Updated project parsing

--- server/src/Lib/Process/Process.ts
+++ server/src/Lib/Process/Process.ts
@@ -2,6 +2,9 @@ import { TextDocument } from "../Types/Document/TextDocument";
 import { Database } from "../Database/Database";
 import { HandleError } from "../Code/Error";
 import { ProvideDiagnostics } from "../Diagnostics/OnRequest";
+import { Languages } from "../Constants";
+import { Traverse } from "./Traverse";
+import { Manager } from '../Manager/Manager';
 
 //Process the given document
 export function Process(document: TextDocument): void {
@@ -12,5 +15,9 @@ export function Process(document: TextDocument): void {
     ProvideDiagnostics(document);
   } catch (error) {
     HandleError(error, document);
+  }
+
+  if (document.languageId === Languages.McProjectIdentifier) {
+    if (Manager.State.TraversingProject === false) Traverse();
   }
 }
