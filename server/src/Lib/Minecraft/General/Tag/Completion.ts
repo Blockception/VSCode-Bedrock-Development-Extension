62e64b6b6d58ef6585e802ff4e29887f26eadc05
DaanV2 <daanverstraten@hotmail.com>
2022-02-09 09:27:06 +0100

    Added empty tag completion

--- server/src/Lib/Minecraft/General/Tag/Completion.ts
+++ server/src/Lib/Minecraft/General/Tag/Completion.ts
@@ -6,18 +6,17 @@ import { Kinds } from "../Kinds";
 
 export function ProvideCompletion(context: SimpleContext<CompletionBuilder>): void {
   const receiver = context.receiver;
+  receiver.Add("Any Tag", "By inserting an `tag=` you test for entities with any kind of tag", Kinds.Completion.Tag, "");
+  receiver.Add("No Tags", "By inserting an `tag=!` you test for entities with no tags", Kinds.Completion.Tag, "!");
 
   receiver.Generate(Database.ProjectData.General.tags, generateDocumentation, Kinds.Completion.Tag);
 
   const data = context.doc.getConfiguration();
-
   receiver.GenerateStr(data.definitions.tag?.defined, generateDocumentation, Kinds.Completion.Tag);
 }
 
 function generateDocumentation(tag: GeneralInfo | string): string {
-  if (typeof tag === "string") {
-    return `The tag: ${tag}`;
-  }
+  if (typeof tag === "string") return `The tag: ${tag}`;
 
   const filename = GetFilename(tag.location.uri);
 
