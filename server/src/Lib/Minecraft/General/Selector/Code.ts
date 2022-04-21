3c84e41e631e67d1f98947ed7479b4ca9e25ddf8
DaanV2 <daanverstraten@hotmail.com>
2022-04-21 12:17:04 +0200

    Updated

---
+++ server/src/Lib/Minecraft/General/Selector/Code.ts
@@ -0,0 +1,17 @@
+import { Minecraft } from 'bc-minecraft-bedrock-diagnoser/node_modules/bc-minecraft-bedrock-types';
+
+
+export function getAttribute(attr : string, selector : string) : string[] {
+    const sel = Minecraft.Selector.Selector.parse(selector);
+    const types = sel.get(attr).map(castToString);
+
+    return types
+}
+
+function castToString(attr : Minecraft.Selector.SelectorAttribute) : string {
+    if (attr instanceof Minecraft.Selector.SelectorValueAttribute) {
+        return attr.getValue().text;
+    }
+
+    return ""
+}
