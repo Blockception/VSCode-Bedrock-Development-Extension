2e95a7bd934bbcbd6239ab97d77d6386f7bcebbd
Daan Verstraten <daanverstraten@hotmail.com>
2023-03-09 16:45:57 +0100

    Reworked showvanilla files (#459)
    * Reworked showvanilla files
    
    * Removed unnesscary trigger

--- client/src/Commands/Vanilla/ShowVanillaFile.ts
+++ client/src/Commands/Vanilla/ShowVanillaFile.ts
@@ -1,39 +1,42 @@
-import path from "path";
-import { commands, ExtensionContext, Uri, window } from "vscode";
-import { Commands, GetBedrockInstallationFolder } from "@blockception/shared";
-import { Glob } from "../../Glob";
+import { commands, ExtensionContext, Uri, window, workspace } from "vscode";
+import { Commands } from "@blockception/shared";
+import { GithubFiles } from "bc-minecraft-bedrock-vanilla-data/lib/src/Lib/Vanilla/sources";
+import fetch, { RequestInit } from "node-fetch";
 
 export function Activate(context: ExtensionContext): void {
-  context.subscriptions.push(commands.registerCommand(Commands.ShowVanillaFile, ShowVanillaFile));
-}
+  async function ShowVanillaFile(args: any) {
+    const source = GithubFiles.source;
+    const files = GithubFiles.files;
 
-function ShowVanillaFile(args: any) {
-  const dir = path.join(GetBedrockInstallationFolder(), "data");
-  const files = Glob.GetFiles(["{behavior_packs,resource_packs}/vanilla**/**/*.json"], ["behavior_trees", "contents.json"], dir);
+    if (files.length === 0) {
+      return;
+    }
 
-  if (files.length === 0) {
-    window.showErrorMessage("No vanilla files found");
-    return;
+    window.showQuickPick(files).then((filepath) => {
+      if (!filepath) {
+        return;
+      }
+
+      const uri = Uri.parse(source + filepath);
+      const options: RequestInit = {
+        method: "GET",
+        redirect: "error",
+      };
+
+      console.log("Downloading vanilla file", filepath)
+      return fetch(uri.toString(), options)
+        .then(data => data.text())
+        .then(text => {
+          workspace.openTextDocument({ language: "json", content: text }).then(doc => window.showTextDocument(doc))
+        })        
+        .catch(err => {
+          window.showErrorMessage("Failed to download vanilla file", filepath as string, JSON.stringify(err));
+        }).finally(() => {
+          console.log("Downloaded vanilla file", filepath)
+        });
+
+    });
   }
 
-  const options: Record<string, string> = {};
-  
-  files.forEach((file) => {
-    const key = path.basename(file);
-    options[key] = file;
-  });
-
-  const keys = Object.keys(options);
-  keys.sort();
-
-  window.showQuickPick(keys).then((key) => {
-    if (key) {
-      let file = options[key];
-      if (file) {
-        const uri = Uri.file(file);
-
-        window.showTextDocument(uri, { preview: false });
-      }
-    }
-  });
+  context.subscriptions.push(commands.registerCommand(Commands.ShowVanillaFile, ShowVanillaFile));
 }
