b52b2969e81c7687f4f2a46e9e1d2c63b2d0e7de
stirante <brzozowski.s.piotr@gmail.com>
2023-10-14 20:07:57 +0200

    Add show docs command

--- client/src/Commands/Activate.ts
+++ client/src/Commands/Activate.ts
@@ -3,10 +3,12 @@ import * as Create from "./Create/Create";
 import * as Errors from "./Errors/OpenLastest";
 import * as Language from "./Language/Activate";
 import * as ShowVanillaFile from "./Vanilla/ShowVanillaFile";
+import * as ShowDocs from "./Docs/ShowDocs";
 
 export function Activate(context: ExtensionContext): void {
   Create.Activate(context);
   Errors.Activate(context);
   Language.Activate(context);
   ShowVanillaFile.Activate(context);
+  ShowDocs.Activate(context);
 }
