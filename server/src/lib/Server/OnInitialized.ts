ce7037db2fb77d6e1c270e0358a42be8f6b0ea0a
DaanV2 <daanverstraten@hotmail.com>
2024-05-26 21:59:40 +0200

    Adding logger, service

--- server/src/lib/Server/OnInitialized.ts
+++ server/src/lib/Server/OnInitialized.ts
@@ -1,26 +1,4 @@
 import { InitializedParams } from "vscode-languageserver";
 import { Console } from "../Manager/Console";
-import { Traverse } from "../Process/Traverse";
 import { SetDynamicEvents } from "./Events/Dynamic";
 import { UpdateSettings } from "./Settings/Update";
-import { HandleError } from '../Code/Error';
-
-export async function onInitializedAsync(params: InitializedParams): Promise<void> {
-  return Promise.resolve(onInitialized(params));
-}
-
-function onInitialized(params: InitializedParams): void {
-  Console.Log("Initialized minecraft server");
-
-  //Update the settings of the language server
-  UpdateSettings();
-
-  //Registers any follow ups
-  SetDynamicEvents();
-
-  //For debug purposes use a higher delay version
-  setImmediate(() => {
-    //Catch error to prevent the server from crashing
-    Traverse().catch((err) => HandleError(err));
-  });
-}
