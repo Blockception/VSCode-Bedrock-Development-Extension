366c01406d56a93ba64a608c7985ca145b84e564
DaanV2 <daanverstraten@hotmail.com>
2024-08-03 16:29:13 +0200

    refactored context

--- server/src/lib/lsp/context/context.ts
+++ server/src/lib/lsp/context/context.ts
@@ -1,6 +1,6 @@
 import { IExtensionContext } from "../extension/context";
 
-export type Context<T> = T & IExtensionContext;
+export type Context<T> = Readonly<T> & IExtensionContext;
 
 export namespace Context {
   export function create<T>(base: IExtensionContext, additional: T, overrides?: Partial<Context<T>>): Context<T> {
@@ -13,9 +13,17 @@ export namespace Context {
       documents: base.documents,
       logger: base.logger,
       state: base.state,
+      settings: base.settings,
 
       ...additional,
       ...overrides,
+    };
+  }
+
+  export function modify<A, B>(base: Context<A>, overlay: B): Context<A & B> {
+    return {
+      ...base,
+      ...overlay,
     };
   }
 }
