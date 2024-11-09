f39b86f2225963d42c0b03e8ec3e65d2440e8d49
Daan Verstraten <daanverstraten@hotmail.com>
2024-11-09 23:07:23 +0100

    Fixing the failing pipelines on linux / macos (#694)
    * adding dev container for codespaces
    
    * updating jest config

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -28,7 +28,7 @@ const config: Config = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
+  coveragePathIgnorePatterns: ["node_modules"],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
